c4e00d2a84f95978a0c4e795e05e480dd8ae20b5
Harsh Maur <harshmaur@gmail.com>
2021-07-01 00:26:20 +0530

    feat(): remove abi and fix test

--- src/test/Inbox.test.ts
+++ src/test/Inbox.test.ts
@@ -2,10 +2,22 @@
 const Inbox = artifacts.require('Inbox')
 
 contract('Inbox', (accounts) => {
-  it('deploys a contract', async () => {
-    console.log(accounts)
+  it('is deployed', async () => {
+    const inboxInstance = await Inbox.deployed()
+    expect(inboxInstance.address).to.exist
+  })
+
+  it('gets initialised with a default value', async () => {
     const inboxInstance = await Inbox.deployed()
     const message = await inboxInstance.message()
     expect(message).to.eq('Yo')
+  })
+
+  it('should set the message', async () => {
+    const newMessage = 'New Message'
+    const inboxInstance = await Inbox.deployed()
+    await inboxInstance.setMessage(newMessage)
+    const message = await inboxInstance.message()
+    expect(message).to.eq(newMessage)
   })
 })
