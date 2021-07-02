bbebc448be5e02f927bf59e31a5ab1208fac34da
Harsh Maur <harshmaur@gmail.com>
2021-07-02 18:36:34 +0530

    feat(): rinkeby deployment setup

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const mnemonic = process.env.WALLET_MNEMONIC
+
 module.exports = {
   networks: {
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, RINKEBY_PROJECT_URI)
+      },
+      network_id: 4,
+    },
     development: {
       host: '127.0.0.1',
       port: 7545,
